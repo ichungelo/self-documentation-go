c07c326c4056e3b7a6726ae393af7666c29ddaef
Krisna Satriadi <krisna.s.362@gmail.com>
2023-02-09 13:10:16 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# SELF DOCUMENTATION GO
+
+## THIS PROJECT IS MADE TO LEARN GO DOCUMENTATION
+
+### INSTALLATION
+
+```shell
+go get github.com/ichungelo/sellf-documentation-go
+```
--- addition.go
+++ addition.go
@@ -1,26 +1,71 @@
 /*
 This Package is used to do math operaion
+
+# Test1
+
+this is a test 1
+
+# Test2
+
+this is a test 2
+
+# Test3
+
+this is a test 3
+
+# Test4
+
+this is a test 4
+
+# Test5
+
+this is a test 5
+
 */
 package selfdocumentationgo
 
+/*
+# Calculate Struct
 
+this struct is used for add float number data which want to do operation
+*/
 type Calculate struct {
 	Num1 float64
 	Num2 float64
 }
 
+/*
+# Addition Method
+
+Used for do addition in calculate
+*/
 func (c Calculate) Addition() float64 {
 	return c.Num1 + c.Num2
 }
 
+/*
+# Subtraction Method
+
+Used for do subtraction in calculate
+*/
 func (c Calculate) Subtraction() float64 {
 	return c.Num1 - c.Num2
 }
 
+/*
+# Multiplication Method
+
+Used for do multiplication in calculate
+*/
 func (c Calculate) Multiplication() float64 {
 	return c.Num1 * c.Num2
 }
 
+/*
+# Division Method
+
+Used for do division in calculate
+*/
 func (c Calculate) Division() float64 {
 	return c.Num1 / c.Num2
 }
